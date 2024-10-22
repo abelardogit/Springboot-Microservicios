de63d5d4c4ddb576f00e048aeebc25c6aa2a420b
Abelardo León González <github@abelardolg.com>
2024-10-22 15:21:32 +0200

    video#26

---
+++ paymentchainparent/businessdomain/product/src/main/java/com/paymentchain/product/controller/ProductRestController.java
@@ -0,0 +1,78 @@
+package com.paymentchain.product.controller;
+
+import java.util.List;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import com.paymentchain.product.controller.helper.ProductRestControllerHelper;
+import com.paymentchain.product.entities.Product;
+import com.paymentchain.product.repository.ProductRepository;
+
+
+
+@RestController
+@RequestMapping("/product")
+public class ProductRestController {
+
+    private final ProductRepository productRepository;
+
+    public ProductRestController(ProductRepository productRepository) {
+        this.productRepository = productRepository;
+    }
+
+    @GetMapping()
+    public List<Product> list()
+    {
+        System.out.println("hola2");
+        return this.productRepository.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<?> getById(@PathVariable("id") long id)
+    {
+        Product aProduct = ProductRestControllerHelper.getById(this.productRepository, id);
+        if (null == aProduct) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+
+        return new ResponseEntity<>(aProduct, HttpStatus.FOUND);
+
+    }
+
+    @PutMapping()
+    public ResponseEntity<?> put(@RequestBody Product product)
+    {
+        long productId = product.getId();
+        Product aProductFromBD = ProductRestControllerHelper.getById(this.productRepository, productId);
+        if (null == aProductFromBD) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+
+        Product updatedProduct = ProductRestControllerHelper.update(aProductFromBD, product);
+
+        this.productRepository.save(updatedProduct);
+
+        return new ResponseEntity<>(updatedProduct, HttpStatus.OK);
+    }
+
+    @PostMapping
+    public ResponseEntity<?> post(@RequestBody Product aProduct)
+    {
+        Product savedProduct = this.productRepository.save(aProduct);
+        return ResponseEntity.ok(savedProduct);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> delete(@PathVariable("id") int id)
+    {
+        Product aProductFromBD = ProductRestControllerHelper.getById(this.productRepository, id);
+        if (null == aProductFromBD) {
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        }
+        this.productRepository.delete(aProductFromBD);
+
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+}
---
+++ paymentchainparent/businessdomain/product/src/main/java/com/paymentchain/product/controller/helper/ProductRestControllerHelper.java
@@ -0,0 +1,45 @@
+package com.paymentchain.product.controller.helper;
+
+import com.paymentchain.product.entities.Product;
+import com.paymentchain.product.repository.ProductRepository;
+
+import java.util.Optional;
+
+public class ProductRestControllerHelper {
+
+    public static Product getById(ProductRepository productRepository, long id)
+    {
+        Optional<Product> optionalProduct = productRepository.findById(id);
+        return optionalProduct.orElse(null);
+    }
+
+    public static Product update(Product fromBD, Product fromUser)
+    {
+        Product updatedProduct = new Product();
+        updatedProduct.setId(fromBD.getId());
+        Product from;
+        if (null != fromUser.getCode()) {
+            from = fromUser;
+        } else {
+            from = fromBD;
+        }
+        updatedProduct.setCode(from.getCode());
+
+        if (null != fromUser.getPhone()) {
+            from = fromUser;
+        } else {
+            from = fromBD;
+        }
+        updatedProduct.setPhone(from.getPhone());
+
+        if (null != fromUser.getName()) {
+            from = fromUser;
+        } else {
+            from = fromBD;
+        }
+        updatedProduct.setName(from.getName());
+
+        return updatedProduct;
+    }
+
+}
