c63f0556a3da2c2c525e2fd8802b843e53470c7b
Abelardo León González <github@abelardolg.com>
2024-10-23 17:19:06 +0200

    Remove validation

--- paymentchainparent/businessdomain/product/src/main/java/com/paymentchain/product/entities/Product.java
+++ paymentchainparent/businessdomain/product/src/main/java/com/paymentchain/product/entities/Product.java
@@ -4,7 +4,6 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
 
 @Entity
@@ -13,8 +12,6 @@ public class Product {
     @GeneratedValue(strategy = GenerationType.AUTO)
     @Id
     private long id;
-    @NotBlank(message="Code is mandatory")
     private String code;
-    @NotBlank(message="Name is mandatory")
     private String name;
 }
