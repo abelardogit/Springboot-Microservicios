83536bd8a642599d92bf3086cea998dd822f8b1e
Abelardo León González <github@abelardolg.com>
2024-11-05 13:24:26 +0100

    Video#60

--- paymentchainparent/businessdomain/customer/src/main/java/com/paymentchain/customer/controller/CustomerRestController.java
+++ paymentchainparent/businessdomain/customer/src/main/java/com/paymentchain/customer/controller/CustomerRestController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/customer")
+@RequestMapping("/customer/v1")
 public class CustomerRestController {
 
     private final CustomerRepository customerRepository;
