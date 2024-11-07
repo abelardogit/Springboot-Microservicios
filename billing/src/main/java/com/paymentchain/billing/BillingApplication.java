65eb37a732754edff2e02e9ec0a352d5ccdb0279
Abelardo León González <github@abelardolg.com>
2024-11-07 11:22:22 +0100

    Video#82

--- billing/src/main/java/com/paymentchain/billing/BillingApplication.java
+++ billing/src/main/java/com/paymentchain/billing/BillingApplication.java
@@ -1,13 +1,17 @@
 package com.paymentchain.billing;
 
+import com.paymentchain.billing.config.SecurityConfiguration;
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.FilterType;
 
-@SpringBootApplication
+@SpringBootApplication()
+@ComponentScan(excludeFilters={@ComponentScan.Filter(type= FilterType.ASSIGNABLE_TYPE, value= SecurityConfiguration.class)})
 public class BillingApplication {
 
 	public static void main(String[] args) {
--- billing/src/test/java/com/paymentchain/billing/BillingApplicationTests.java
+++ billing/src/test/java/com/paymentchain/billing/BillingApplicationTests.java
@@ -1,7 +1,10 @@
 package com.paymentchain.billing;
 
+import com.paymentchain.billing.config.SecurityConfiguration;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.FilterType;
 
 @SpringBootTest
 class BillingApplicationTests {
